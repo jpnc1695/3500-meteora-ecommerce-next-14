9d03d306ed902f3c1d687286323ee030318a91d1
gss-patricia <gss.patricia@gmail.com>
2024-02-22 22:36:52 +0000

    video 2.2

--- src/app/produto/[slug]/page.jsx
+++ src/app/produto/[slug]/page.jsx
@@ -1,9 +1,16 @@
 import styles from "./page.module.css";
 import Produto from "@/app/components/Produto";
-import { getProdutoPorSlug } from "../../../lib/api";
+
+async function getProduto(slug) {
+  const res = await fetch(`http://localhost:3000/api/produto/${slug}`);
+
+  const produto = await res.json();
+
+  return produto;
+}
 
 export default async function ProdutoPage({ params }) {
-  const produto = getProdutoPorSlug(params.slug);
+  const { produto } = await getProduto(params.slug);
 
   return (
     <main className={styles.main}>
