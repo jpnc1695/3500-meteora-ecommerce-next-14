c7dfd1b6f6fda4105e45ffd70d5ca490da85aca2
jpnc1695 <joaopaulonunescosta@gmail.com>
2025-02-10 15:52:24 -0300

    atualizacao next Config e criacao de imagem.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  assetPrefix: "/out",
+  trailingSlash: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "raw.githubusercontent.com",
-        pathname: "**",
-      },
-    ],
+    loader: "custom",
+    loaderFile: "./src/app/imagem.js"
   },
 };
 
